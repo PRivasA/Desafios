38f4ec69cf746f7e131f549fe6f239965cc98be0
PRivasA <106940501+PRivasA@users.noreply.github.com>
2023-03-03 18:41:53 -0800

    Add files via upload

---
+++ Incio_sesion.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Incio_sesion
+{
+    internal class program
+    {
+        static string nombre="";
+        static string contrasena="";
+
+        static void Main(string[] args)
+        {
+            Registrar();
+            IniciarSesion();
+
+            Console.ReadKey();
+   
+        }
+
+
+        static void Registrar() {
+            Console.WriteLine("Registro Ingrese nombre del usuario:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Registro Ingrese contrasena del usuario:");
+            contrasena = Console.ReadLine();
+        }
+        static void IniciarSesion()
+        {
+            Console.WriteLine("Login Ingrese nombre del usuario:");
+            string usuarioIngresado = Console.ReadLine();
+            Console.WriteLine("login Ingrese contrasena del usuario:");
+            string contrasenaIngresada = Console.ReadLine();
+
+            if (nombre.ToLower().Equals(usuarioIngresado.ToLower()))
+            {
+                if (contrasena.Equals(contrasenaIngresada))
+                {
+                    Console.WriteLine("Bienvenido {0}", usuarioIngresado);
+                }
+                else
+                {
+                    Console.WriteLine("contrasena invalida");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El usuario es invalido");
+            }
+        }
+    }
+}
---
+++ Operaciones_basicas.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Operaciones_basicas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedirDatos();
+            Calculadora calculadora = new Calculadora();
+
+            int total = calculadora.Sumar(10, 20);
+            int diferencia = calculadora.Restar(10, 20);
+            int producto = calculadora.Multiplicar(10, 20);
+            int cociente = calculadora.Dividir(100, 20);
+
+
+            Console.WriteLine("Los resultados son suma={0}, resta={1}, multiplicacion={2}, division={3}", total, diferencia, producto, cociente);
+            Console.ReadKey();
+
+        }
+    }
+}
