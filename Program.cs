23a8f22765e323db72b179ca81b6ab88f9010545
PRivasA <106940501+PRivasA@users.noreply.github.com>
2023-03-03 16:21:53 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-ConvertirAEntero();
-Console.ReadKey();
+using System;
 
-static void ConvertirAEntero()
+public class Program
 {
-    Console.WriteLine("Ingrese 2 numeros");
-    try
+    static int Puntaje = 0;
+    static int PuntuacionRecord = 0;
+    static string JugadorRecord = "";
+
+    static void Main(string[] args)
     {
-        int n1 = int.Parse(Console.ReadLine());
-        int n2 = int.Parse(Console.ReadLine());
-        int total = n1 + n2;
-        Console.WriteLine("El resultado es: {0}", total);
+        Console.WriteLine("Ingrese el nombre del jugador 1: ");
+        string NombredelJugador1 = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el puntaje de jugador 1: ");
+        int PuntuaciondelJugador1 = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el nombre de jugador 2: ");
+        string NombredelJugador2 = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el puntaje de jugador 2: ");
+        int PuntuaciondelJugador2 = Int32.Parse(Console.ReadLine());
+
+        CheckScore(PuntuaciondelJugador1, NombredelJugador1);
+        CheckScore(PuntuaciondelJugador2, NombredelJugador2);
     }
-    catch (Exception c)
+
+    static void CheckScore(int Puntaje, string Nombre)
     {
-        Console.WriteLine("Ocurrio un error");
-    }
-    finally
-    {
-        Console.WriteLine("Programa terminado");
+        if (Puntaje > PuntuacionRecord)
+        {
+            PuntuacionRecord = Puntaje;
+            JugadorRecord = Nombre;
+
+            Console.WriteLine("La nueva puntuación más alta es " + PuntuacionRecord);
+            Console.WriteLine("La puntuación más alta fue lograda por " + JugadorRecord);
+        }
+        else
+        {
+            Console.WriteLine("La puntuación más alta de " + PuntuacionRecord + " no se ha podido superar, y aún está en manos de " + JugadorRecord);
+        }
     }
 }
-
